2fda6e3e80180362904fedadbaa5aa2949c6915d
Gabriel <p.set57@gmail.com>
2019-06-30 21:34:47 -0300

    RobinCamera Inicio

--- robinlibrary/src/main/java/Service/RobinCamera.kt
+++ robinlibrary/src/main/java/Service/RobinCamera.kt
@@ -1,4 +1,44 @@
 package Service
 
+import Util.Const
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.os.Build
+import android.support.v4.app.ActivityCompat
+import android.support.v4.app.Fragment
+
+
 class RobinCamera {
+    private var act: Activity? = null
+        get() {
+            return if (field == null) frag.activity else field
+        }
+
+    private lateinit var frag: Fragment
+
+    constructor(activity: Activity) {
+        this.act = act
+    }
+
+    constructor(fragment: Fragment) {
+        this.frag = fragment
+    }
+
+    val isPermission: Boolean
+        @SuppressLint("NewApi")
+        get() {
+            return act!!.checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+        }
+
+    @SuppressLint("NewApi")
+    fun requestPermission() {
+        if (!isPermission) {
+            act!!.requestPermissions(
+                arrayOf(Manifest.permission.CAMERA),
+                Const.REQUEST_CAMERA
+            )
+        }
+    }
 }
---
+++ robinlibrary/src/main/java/Util/Const.kt
@@ -0,0 +1,10 @@
+package Util
+
+class Const {
+    companion object {
+        //region Camera
+        val PERMISSAO_CAMERA = 101
+        val REQUEST_CAMERA = 102
+        //endregion Camera
+    }
+}
